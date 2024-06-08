32047335c6df0b8215122776452d5153a69015ca
xzhc <1838998336@qq.com>
2024-06-08 17:43:07 +0800

    feat(vite-config): enhance config with new plugins and server settings
    - Integrate @vitejs/plugin-vue, @vitejs/plugin-vue-jsx for Vue support.
    - Add unplugin-auto-import for automatic imports of Vue components and utilities.
    - Set up unplugin-vue-components for component resolution using NaiveUiResolver.
    - Implement vite-plugin-compression for compression.
    - Update server configuration with a specific port and auto-open browser on start.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
+    Components({ resolvers: [NaiveUiResolver()] }),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
+        }
+      ]
+    }),
+    viteCompression()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 5174,
+    open: 'http://localhost:5174/'
   }
 })
